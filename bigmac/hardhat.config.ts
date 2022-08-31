3e3e956b789e6a849af58e413ca25b38cce74acf
Andrej <andrej.rakic@elfak.rs>
2022-09-01 01:12:39 +0200

    Add test scenario for BigMac raffle (#3)

---
+++ bigmac/hardhat.config.ts
@@ -0,0 +1,35 @@
+import * as dotenv from "dotenv";
+
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.7",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        url: process.env.GOERLI_RPC_URL !== undefined ? process.env.GOERLI_RPC_URL : ``,
+        blockNumber: 7504575,
+      },
+    },
+    goerli: {
+      url: process.env.GOERLI_RPC_URL !== undefined ? process.env.GOERLI_RPC_URL : ``,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
+
+export default config;
