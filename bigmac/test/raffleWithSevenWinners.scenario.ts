b1f7eb8406767cf92eae9c27319cb9c31ea2d201
Andrej <andrej.rakic@smartcontract.com>
2022-09-13 14:43:27 +0200

    Remove _addParticipants function, code cleanup (#7)

--- bigmac/test/raffleWithSevenWinners.scenario.ts
+++ bigmac/test/raffleWithSevenWinners.scenario.ts
@@ -2,7 +2,7 @@ import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { assert, expect } from "chai";
 import { BigNumber, constants, ContractReceipt, ContractTransaction } from "ethers";
-import { parseEther } from "ethers/lib/utils";
+import { BytesLike, parseEther } from "ethers/lib/utils";
 import { ethers, network } from "hardhat";
 import { BigMacRaffle, VRFCoordinatorV2Mock } from "../typechain-types";
 
@@ -29,8 +29,24 @@ describe(`BigMac Raffle test scenario with seven winners on Goerli testnet`, asy
     const requestConfirmations = 5;
     const numWords = 7;
 
+    const mockParticipants: BytesLike[] = [
+      `0x641b11da408590705534ed5490f73a3b2a67b5a863e46bc5d91322f67eea15ef`,
+      `0xa19feb02fec238cf38a8b3c705f86a9e5459257a9a41e25dd8ebbcb0c6aaab6e`,
+      `0xc2296d861dba29a8cbc07c736692f2cd6c4a24ee7a9b4528a338271769c86c79`,
+      `0x8988f55288729a137e5003981a56db0c5eb2a400be05c87501ea16107642b175`,
+      `0xc120b4d20493030bfca3b5d1f85287dc2715e87e40ad0ecfaa87241cf4412a74`,
+      `0x2aaa45affb34e5ccdfece5ca9dff4dd3014a5651f9b44123489b12dcad83d2ac`,
+      `0x29687b53a1814b9496b7a141660054c835c0324e4f0851e849e5aa6c640357ee`,
+      `0xab6c0333b6544caece6087ee699940b1cb3c6b1b696690dbdfc45c92e23fc414`,
+      `0x3c023a5081ae97fb4fa214d233e4831c61168b41be5bb6c1131e42fbb29622ec`,
+      `0x58bc086d6c0abed01c03e3d7d5489289035ad9afa7d496f4d3fdc5d0216e2279`,
+      `0x4121e62e50c9b6345f60f9647274947689c2f1450d413010531061d303082724`,
+      `0x680e9ac1b62d2ad60a81cbc5432a924345ff0a1512286411bbc6a7734578169a`,
+    ];
+
     const bigMacRaffleFactory = await ethers.getContractFactory(`BigMacRaffle`);
     const bigMacRaffle: BigMacRaffle = (await bigMacRaffleFactory.deploy(
+      mockParticipants,
       subscriptionId,
       mockVrfCoordinator.address,
       keyHash,
